e19d14194cc780b1ad96d4f9777ee3a0797206d7
macv101 <michaelvillar0012@gmail.com>
2015-11-25 23:33:15 -0800

    JaxRsActivator added

---
+++ src/main/java/com/read60/rest/JaxRsActivator.java
@@ -0,0 +1,9 @@
+package com.read60.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class JaxRsActivator extends Application {
+
+}
